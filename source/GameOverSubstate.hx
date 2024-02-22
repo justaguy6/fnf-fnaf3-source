5f1f8e6873b6640238d1dbaa439222eecfa420c5
Paulo Ricardo <100438552+TerrificCalvach@users.noreply.github.com>
2024-02-21 22:25:03 -0300

    Update GameOverSubstate.hx

--- source/GameOverSubstate.hx
+++ source/GameOverSubstate.hx
@@ -37,6 +37,10 @@ class GameOverSubstate extends MusicBeatSubstate
 		instance = this;
 		PlayState.instance.callOnLuas('onGameOverStart', []);
 
+		#if android
+		addVirtualPad(NONE, A_B);
+		#end
+
 		super.create();
 	}
 
@@ -104,11 +108,7 @@ class GameOverSubstate extends MusicBeatSubstate
 			Conductor.songPosition = FlxG.sound.music.time;
 		}
 		PlayState.instance.callOnLuas('onUpdatePost', [elapsed]);
-
-		#if android
-		addVirtualPad(NONE, A_B);
-		addPadCamera();
-		#end
+		
 	}
 
 	override function beatHit()
