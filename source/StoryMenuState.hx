d4960587e8ea3a13cc9a0d5b368b71094c233da9
Paulo Ricardo <100438552+TerrificCalvach@users.noreply.github.com>
2024-02-21 22:11:13 -0300

    Update StoryMenuState.hx

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -114,11 +114,6 @@ class StoryMenuState extends MusicBeatState
 		startButton.x = 535;
 		startButton.y = 385;
 
-		#if android
-		addVirtualPad(NONE, A);
-		addPadCamera();
-		#end
-
 		FlxG.mouse.visible = true;
 	}
 	
