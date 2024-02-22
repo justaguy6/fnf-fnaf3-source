cd6f857e3454a126ae0194a55da4e1555c7b0d15
Paulo Ricardo <100438552+TerrificCalvach@users.noreply.github.com>
2024-02-21 22:53:38 -0300

    Controles 😎

--- source/Controls.hx
+++ source/Controls.hx
@@ -747,6 +747,27 @@ class Controls extends FlxActionSet
 		#end
 	}
 
+	#else
+
+	public function bindKeys(control:Control, keys:Array<FlxKey>)
+	{
+		#if (haxe >= "4.0.0")
+		inline forEachBound(control, (action, state) -> addKeys(action, keys, state));
+		#else
+		forEachBound(control, function(action, state) addKeys(action, keys, state));
+		#end
+	}
+
+	public function unbindKeys(control:Control, keys:Array<FlxKey>)
+	{
+		#if (haxe >= "4.0.0")
+		inline forEachBound(control, (action, _) -> removeKeys(action, keys));
+		#else
+		forEachBound(control, function(action, _) removeKeys(action, keys));
+		#end
+	}	
+	#end
+	
 	inline static function addKeys(action:FlxActionDigital, keys:Array<FlxKey>, state:FlxInputState)
 	{
 		for (key in keys)
